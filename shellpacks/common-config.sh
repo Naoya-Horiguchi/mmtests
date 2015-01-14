570d3b47b42dac033a5af07ea0554d078945a987
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2015-01-14 23:21:51 +0900

    setup local WEBROOT and kernel version for kernbench

--- shellpacks/common-config.sh
+++ shellpacks/common-config.sh
@@ -7,6 +7,6 @@ export SHELLPACK_TEMP=$SHELLPACK_TEST_MOUNT/tmp/$$
 export SHELLPACK_INCLUDE=$SCRIPTDIR/shellpacks
 export SHELLPACK_LOG=$SCRIPTDIR/work/log
 export LINUX_GIT=/src/linux-dev
-export WEBROOT=http://mcp/mmtests-mirror
+export WEBROOT=http://nfssv.rhts.fuchu/export/data/user/n-horiguchi/mmtests-mirror
 export SSHROOT=`echo $WEBROOT | sed -e 's/http:\/\//ssh:\/\/root@/'`/public_html
 export TESTDISK_DIR

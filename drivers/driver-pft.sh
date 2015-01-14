ca3a24d2ff98800764a001d3b46c62f280caba62
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2015-01-14 23:21:52 +0900

    pft: support hugetlb

--- drivers/driver-pft.sh
+++ drivers/driver-pft.sh
@@ -4,13 +4,24 @@ NAMEEXTRA=
 run_bench() {
 	LOGDIR_TOPLEVEL=$LOGDIR_RESULTS
 	for PAGESIZE in $PFT_PAGESIZES; do
-		unset USELARGE
 		unset USE_DYNAMIC_HUGEPAGES
+                unset PAGEPARAM
 		case $PAGESIZE in
 		base)
+			PAGEPARAM=--smallonly
+			disable_transhuge
+			;;
+		huge)
+			PAGEPARAM=--hugeonly
+			disable_transhuge
+			;;
+		dynhuge)
+			PAGEPARAM=--hugeonly
+			export USE_DYNAMIC_HUGEPAGES=yes
 			disable_transhuge
 			;;
 		transhuge)
+			PAGEPARAM=--smallonly
 			if [ "$TRANSHUGE_AVAILABLE" = "yes" ]; then
 				enable_transhuge
 			else
@@ -25,7 +36,7 @@ run_bench() {
 
 		export LOGDIR_RESULTS=$LOGDIR_TOPLEVEL/$PAGESIZE
 		mkdir -p $LOGDIR_RESULTS
-		$SHELLPACK_INCLUDE/shellpack-bench-pft
+		$SHELLPACK_INCLUDE/shellpack-bench-pft $PAGEPARAM
 		RETVAL=$?
 	done
 	export LOGDIR_RESULTS=$LOGDIR_TOPLEVEL
